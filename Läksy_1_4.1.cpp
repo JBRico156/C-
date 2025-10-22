4fc082ef6653d28ffcfb0dd69e2d52b493a8d4c8
Jan B <janbarra@hotmail.com>
2025-10-22 15:34:16 +0300

    Add files via upload

---
+++ Läksy_1_4.1.cpp
@@ -0,0 +1,14 @@
+//Kirjoita while - toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int i = 0;
+	while (i < 1000) 
+	{
+		if (i % 3 == 0)
+			std::cout << i << "\n";
+		i++;
+	}
+}
