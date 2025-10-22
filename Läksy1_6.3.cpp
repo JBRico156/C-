b51e990d89178ab41ea90c696e3731e327161afb
Jan B <janbarra@hotmail.com>
2025-10-22 16:07:55 +0300

    Update Läksy1_6.3.cpp

--- Läksy1_6.3.cpp
+++ Läksy1_6.3.cpp
@@ -1,6 +1,6 @@
-//Kirjoita funktio, joka saa parametrinaan bensiinin m‰‰r‰n Yhdysvaltain nestegallonoina ja palauttaa
-//paluuarvonaan vastaavan litram‰‰r‰n.Kirjoita p‰‰ohjelma, joka kysyy gallonam‰‰r‰n k‰ytt‰j‰lt‰ ja muuntaa sen litroiksi.
-//Muunnos on teht‰v‰ aliohjelmaa hyˆdynt‰en.Muuntamista jatketaan siihen saakka, kunnes k‰ytt‰j‰ syˆtt‰‰ negatiivisen gallonam‰‰r‰n.
+//Kirjoita funktio, joka saa parametrinaan bensiinin m√§√§r√§n Yhdysvaltain nestegallonoina ja palauttaa
+//paluuarvonaan vastaavan litram√§√§r√§n.Kirjoita p√§√§ohjelma, joka kysyy gallonam√§√§r√§n k√§ytt√§j√§lt√§ ja muuntaa sen litroiksi.
+//Muunnos on teht√§v√§ aliohjelmaa hy√∂dynt√§en.Muuntamista jatketaan siihen saakka, kunnes k√§ytt√§j√§ sy√∂tt√§√§ negatiivisen gallonam√§√§r√§n.
 
 //Yksi gallona on 3, 785 litraa.
 #include <iostream>
@@ -12,12 +12,12 @@ int main()
 
     while (true)
     {
-        std::cout << "Anna gallona m‰‰r‰, joka muutetaan lirtoiksi(negatiivinen luku pys‰ytt‰‰ ohjelman): ";
+        std::cout << "Anna gallona m√§√§r√§, joka muutetaan lirtoiksi(negatiivinen luku pys√§ytt√§√§ ohjelman): ";
         std::cin >> gallona;
 
         if (gallona < 0)
         {
-            std::cout << "Kiitos ja n‰kemiin!" << std::endl;
+            std::cout << "Kiitos ja n√§kemiin!" << std::endl;
             break;
         }
 
@@ -26,5 +26,3 @@ int main()
 
     return 0;
 }
-kemiin!")
-            */
