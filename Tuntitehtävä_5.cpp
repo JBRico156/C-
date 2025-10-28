c29f16b68d92bfd9bee9c87b90f455da6d4d8eea
Jan B <janbarra@hotmail.com>
2025-10-28 14:05:20 +0200

    Add files via upload

---
+++ Tuntitehtävä_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int x = 100;
+
+	int* y = &x; //y osoittaa x:‰‰n
+	int** z = &y; // z osoittaa y:hyn
+
+	**z = 200; // muuttaa x:n arvon
+	cout << x << "\n";
+}
